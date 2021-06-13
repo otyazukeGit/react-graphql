128c2f3cc62a7aaadee10c9d85b46f61e7336c68
uuser <otyazuke.gookle@gmail.com>
2021-06-13 18:56:20 +0900

    simple execute ApolloClient

--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  // ApolloProvider,
+  // useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  // uri: 'http://localhost:4000',   // express-graphql localhost:4000
+  cache: new InMemoryCache()
+});
+
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +42,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+reportWebVitals(console.log)
